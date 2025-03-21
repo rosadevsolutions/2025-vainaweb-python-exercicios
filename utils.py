a0412973f90ece271d9ad5c14187f35308832c08
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-20 22:40:20 -0300

    utils resolucao rosadev

---
+++ utils.py
@@ -0,0 +1,13 @@
+#Função pra criar separador
+def separador():
+  print(f'-' * 40)
+
+#Função pra criar separador
+def separador_titulo():
+  print(f'=' * 40)
+
+#Função pra criar o cabeeçalho do exercício
+def cabecalho_exercicio(titulo):
+  separador_titulo()
+  print(titulo.upper())
+  separador_titulo()
