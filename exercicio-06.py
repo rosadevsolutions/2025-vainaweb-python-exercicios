14dec8a4adb8590d14218c7758d1ceedc0d7ab10
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-23 18:54:32 -0300

    exercicio 06 enunciado

---
+++ exercicio-06.py
@@ -0,0 +1,18 @@
+'''
+Sexto Exercício: Combinação de dicionários
+Escreva uma função que recebe dois dicionários
+onde as chaves são strings e os valores são inteiros.
+A função deve combinar os dicionários
+somando os valores das chaves que aparecem em ambos.
+Exemplo:
+d1 = {"a": 2, "b": 3, "c": 5}
+d2 = {"a": 1, "b": 4, "d": 7}
+Saída: {"a": 3, "b": 7, "c": 5, "d": 7}
+'''
+
+#importanto as funções do arquivo utils
+import utils
+
+d1 = {"a": 2, "b": 3, "c": 5}
+d2 = {"a": 1, "b": 4, "d": 7}
+Saída = {}
