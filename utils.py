28dcbe2e75810da01b13a18dd678f2489f262a3d
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-24 14:25:37 -0300

    utils def pular_linha

--- utils.py
+++ utils.py
@@ -6,8 +6,12 @@ def separador():
 def separador_titulo():
   print(f'=' * 40)
 
+def pular_linha():
+  print("\n")
+
 #Função pra criar o cabeeçalho do exercício
 def cabecalho_exercicio(titulo):
+  pular_linha()
   separador_titulo()
   print(titulo.upper())
   separador_titulo()
