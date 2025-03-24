2c08114652d707c50a908aaa11f38d1d555f1b6a
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-24 14:26:29 -0300

    exercicio 01 colocando no padrão de exibicao com utils

--- exercicio-01.py
+++ exercicio-01.py
@@ -6,6 +6,9 @@ Exemplo: [2,4,10,3,9,7,15,22]
 Saída: A soma dos pares é: x
 '''
 
+#importanto as funções do arquivo utils
+import utils
+
 in_numeros_lista = [2,4,10,3,9,7,15,22]
 
 def proc_numeros_pares_calc(numeros):
@@ -13,7 +16,8 @@ def proc_numeros_pares_calc(numeros):
   for numero in numeros:
     if(numero % 2 == 0):
       out_numeros_pares_soma = out_numeros_pares_soma + numero
-  print(f'A soma dos pares é: {out_numeros_pares_soma}')
+  return print(f'A soma dos pares é: {out_numeros_pares_soma}')
 
-
+utils.cabecalho_exercicio('1) Soma de elementos pares')
 proc_numeros_pares_calc(in_numeros_lista)
+utils.pular_linha()
