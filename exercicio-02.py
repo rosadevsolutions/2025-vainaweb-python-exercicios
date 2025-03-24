b5c2c636ea1054057debe3cee6a373f16872be63
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-24 14:28:30 -0300

    exercicio 02 colocando no padrão de exibicao com utils

--- exercicio-02.py
+++ exercicio-02.py
@@ -7,10 +7,15 @@ Exemplo: (“Samuel”, ‘Karynne”, “Carol”, “Kleber”, “Vinicius”
 Saída: (“Carol”, “Karynne”, “Kleber”, “Samuel”, “Vinicius”)
 '''
 
+#importanto as funções do arquivo utils
+import utils
+
 vainawebers = [("Samuel", 20),("Karynne", 22),("Carol", 21),("Kleber", 23),("Vinicius", 25)]
 
 def vainawebers_crescente(lista):
   lista_crescente = sorted(lista, key=lambda item: item[1])
   return print(lista_crescente)
 
+utils.cabecalho_exercicio('2) Ordenação de Tuplas')
 vainawebers_crescente(vainawebers)
+utils.pular_linha()
