95766ba2899c5c8606ec4e6355ab8f4729fade84
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-24 20:49:14 -0300

    exercicio 03 refeito com base na correção do samuca

--- exercicio-03.py
+++ exercicio-03.py
@@ -1,7 +1,6 @@
 '''
 Terceiro Exercício: Contagem de Frequência
-Escreva uma função que
-recebe uma string
+Escreva uma função que recebe uma string
 e retorna um dicionário
 onde as chaves são os caracteres da string
 e os valores representam quantas vezes cada caractere aparece.
@@ -10,28 +9,30 @@ Exemplo: ['Java', 'Java', 'Ruby', 'Javascript', 'Ruby']
 Saída: {'Java': 2, 'Ruby': 2, 'Javascript': 1}
 '''
 
-#importanto as funções do arquivo utils
+#importando as funções comuns criadas do arquivo utils
 import utils
 
-in_linguagens = ['Java', 'Java', 'Ruby', 'Javascript', 'Ruby']
+#declarar função passando um parametro
+def contador_frequencia(in_str):
+  #criar dicionário vazio para depois alimentar e exibir como resultado
+  out_dict = {}
 
-def contador_frequencia(lista):
-  out_contador_java = 0
-  out_contador_ruby = 0
-  out_contador_javascript = 0
-  out_dic_linguagens = {}
+  #Param será uma string que será convertida em lista
+  proc_list = in_str.split(' ')
 
-  for item in lista:
-    if item == 'Java':
-      out_contador_java+=1
-    elif item == 'Ruby':
-      out_contador_ruby+=1
-    else:
-      out_contador_javascript+=1
+  #iterar lista
+  for item in proc_list:
+    #verificar se o item não consta como chave no dicionario
+    if not item in out_dict.keys():
+      #contar o item de lista e atribuir ao dicionario
+      out_dict[item] = proc_list.count(item)
+  #exibir o dicionarioo
+  return print(out_dict)
 
-  out_dic_linguagens = {'Java' : out_contador_java, 'Ruby' : out_contador_ruby, 'Javascript' : out_contador_javascript}
-  return out_dic_linguagens
-
+#string a passar como param quando chamar a função criada a cima
+in_linguagens = 'Java Javascript Java Python C# Java Javascript Go Python Python Go'
 
 utils.cabecalho_exercicio('3) Contagem de Frequência')
-print(contador_frequencia(in_linguagens))
+#chamando a função e passando o parametro
+contador_frequencia(in_linguagens)
+utils.pular_linha()
