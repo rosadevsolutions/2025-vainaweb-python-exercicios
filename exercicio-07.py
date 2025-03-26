b3e3f648fd45bfb03e5be661a8cf16bb001cc4f6
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-26 20:59:54 -0300

    exercicio 07 comentarios

--- exercicio-07.py
+++ exercicio-07.py
@@ -18,20 +18,27 @@ import utils
 def verificar_top_3_frequentes(in_list: list):
   #criar um dict pra contar alimentar com k,v
   proc_dict = {}
+
+  #iterar o param
   for item in in_list:
+    #verificar se o item consta entre as chaves da nova dict
     if item in proc_dict.keys():
+      #se já houver a chace somar 1
       proc_dict[item] += 1
+    #se não constar entre as chaves
     else:
+      #apenas setar como 1
       proc_dict[item] = 1
 
-  proc_dict_sorted = sorted(proc_dict.keys(), key=lambda v:(proc_dict[v], v), reverse=True)
-  out_dict = proc_dict_sorted[:3]
-  #out_dict = sorted( out_dict.keys(), key = lambda chave: (out_dict[chave], chave))
-  return print(out_dict)
+  #variavel que recebe a lista ordenada com os valores decrescentes e exibindo as chaves
+  proc_list_sorted = sorted(proc_dict.keys(), key=lambda v:(proc_dict[v], v), reverse=True)
+  #variavel de saida com a lista acima com corte para exibir só 3 itens
+  out_list = proc_list_sorted[:3]
+  #retorno com a lista final
+  return print(out_list)
 
 in_lista_numeros = [1, 3, 3, 2, 1, 1, 4, 4, 4, 2, 2, 2, 5, 5]
 
 utils.cabecalho_exercicio("Top 3 mais frequentes")
 verificar_top_3_frequentes(in_lista_numeros)
 utils.pular_linha()
-#Saída: [2, 1, 4]
