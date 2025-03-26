cdc5886eab871e0aa3e6d2281b6bdd372908a20b
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-26 14:39:33 -0300

    exercicio 06 usando convencao pytjhon

--- exercicio-06.py
+++ exercicio-06.py
@@ -17,18 +17,18 @@ d1 = {"a": 2, "b": 3, "c": 5}
 d2 = {"a": 1, "b": 4, "d": 7}
 
 #criar função que passa por params 2 dicionarios
-def combinar_dicionarios(dict1, dict2):
+def combinar_dicionarios(in_dict_1, in_dict_2):
   #crio o dicionario para ser alimentado com a fusão
-  proc_dict3 = {}
+  proc_dict_3 = {}
 
   #somo os valores com chaves iguais e mesclando com os não repetidos
-  proc_dict3 = {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1) | set(dict2)}
+  proc_dict_3 = {k: in_dict_1.get(k, 0) + in_dict_2.get(k, 0) for k in set(in_dict_1) | set(in_dict_2)}
 
   #reordeno o dicionario pela chave
-  out_dict3_sorted = dict(sorted(proc_dict3.items(), key= lambda item : item[0]))
+  out_dict_3_sorted = dict(sorted(proc_dict_3.items(), key= lambda item : item[0]))
 
   #retorno com a exibição no console do dicionario reordenado
-  return print(out_dict3_sorted)
+  return print(out_dict_3_sorted)
 
 
 
