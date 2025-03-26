9801b6c021c778e305d862367c80f3bbd484df55
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-26 19:07:45 -0300

    exercicio 06 usando convencao python e ideias da correcao so samuel

--- exercicio-06.py
+++ exercicio-06.py
@@ -13,25 +13,31 @@ Saída: {"a": 3, "b": 7, "c": 5, "d": 7}
 #importanto as funções do arquivo utils
 import utils
 
-d1 = {"a": 2, "b": 3, "c": 5}
-d2 = {"a": 1, "b": 4, "d": 7}
+d1 = {"a": 2, "b": 3, "c": 5, "e": 10}
+d2 = {"a": 1, "b": 4, "d": 7, "x": 60}
 
-#criar função que passa por params 2 dicionarios
-def combinar_dicionarios(in_dict_1, in_dict_2):
-  #crio o dicionario para ser alimentado com a fusão
-  proc_dict_3 = {}
+#criar função passando por params 2 dicionarios
+def combinar_dicts(in_dict_1: dict, in_dict_2: dict):
+  #atribuir os valores do dict 1 ao novo dict
+  proc_dict = in_dict_1
 
-  #somo os valores com chaves iguais e mesclando com os não repetidos
-  proc_dict_3 = {k: in_dict_1.get(k, 0) + in_dict_2.get(k, 0) for k in set(in_dict_1) | set(in_dict_2)}
+  #percorrer todos os items de dict 1 - declarar chave e valor próprios
+  for k, v in in_dict_2.items():
+    #verificar se chave de dict 1 e 2 são iguais
+    if k in proc_dict.keys():
+      #se forem, somar os valores das chaves 1 e 2 e atribuir ao novo dicto
+      proc_dict[k] += v
+    #se não forem iguais
+    else:
+      #atribuir valor referente a chave 2 ao novo dict
+      proc_dict[k] = v
+  #declarar var de saida do dict de saida reordenando pela chave
+  out_dict = dict(sorted(proc_dict.items(), key= lambda item : item[0]))
 
-  #reordeno o dicionario pela chave
-  out_dict_3_sorted = dict(sorted(proc_dict_3.items(), key= lambda item : item[0]))
-
-  #retorno com a exibição no console do dicionario reordenado
-  return print(out_dict_3_sorted)
-
+  #retorno exibe no console o valor do novo dict
+  return(print(out_dict))
 
 
 utils.cabecalho_exercicio('6) Combinação de dicionários')
-combinar_dicionarios(d1, d2)
+combinar_dicts(d1, d2)
 utils.pular_linha()
